16ae5389e6963e7e9c05f5e64392b77aba943591
Artem <artem@wildapricot.com>
2020-05-20 22:46:56 +0300

    add card mocks

--- events-swiftui-prototype/events-swiftui-prototype/Presentation/DashboardCard.swift
+++ events-swiftui-prototype/events-swiftui-prototype/Presentation/DashboardCard.swift
@@ -9,13 +9,27 @@ struct DashboardCard: View {
     let props: DashboardCardProps
 
     var body: some View {
-        ZStack {
+        VStack(alignment: .leading, spacing: 0) {
             Image(props.imgUrl)
                     .resizable()
                     .scaledToFill()
                     .frame(minWidth: nil, idealWidth: nil, maxWidth: UIScreen.main.bounds.width, minHeight: nil, idealHeight: nil, maxHeight: 300, alignment: .center)
                     .clipped()
-            Text(props.name)
+            VStack(alignment: .leading, spacing: 6) {
+                Text(props.name)
+                Text("Category: Light")
+                Text("Location: Izhevsk")
+                Divider()
+                HStack {
+                    Text("Price: 1500₽ per person")
+                    Spacer()
+                    Text("Max pax: 3")
+                }
+            }
+                    .padding(8)
         }
+                .background(Color.white)
+                .cornerRadius(15)
+                .shadow(color: Color.black.opacity(0.2), radius: 7, x: 0, y: 2)
     }
 }
